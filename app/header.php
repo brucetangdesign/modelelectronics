e078446079432bfe4117e280c2feeda2e7029763
Bruce Tang <bruce@brucetangdesign.com>
2022-04-19 17:17:18 -0400

    Header complete

--- app/header.php
+++ app/header.php
@@ -8,5 +8,34 @@
       </div>
     </div>
   </div>
-  <nav></nav>
+  <div id="header-logo-and-nav" class="container-fluid">
+    <div class="container p-0 header-logo-and-nav-contents m-auto">
+      <nav class="navbar navbar-expand-lg d-flex justify-content-between align-items-center p-0">
+        <div class="d-flex align-items-center">
+          <button class="navbar-toggler collapsed me-2 p-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation"></button>
+          <img id="site-identity" src="images/logo-model-electronics.svg" width="226px" height="63px" alt="Model Electronics Logo">
+        </div>
+        <a id="dealer-login-btn-mobile" class="btn btn-primary text-white d-lg-none" href="#" >Dealer Login</a>
+        <div class="collapse navbar-collapse" id="navbarToggler">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Services</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About Us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contact Us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#" >Aftermarket Products</a>
+            </li>
+            <li class="nav-item ms-2 d-none d-lg-block">
+              <a class="nav-link btn btn-primary text-white" href="#" >Dealer Login</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  </div>
 </header>
