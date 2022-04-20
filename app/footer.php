607bfa61ca3580c569a479aea54c41330006ed48
Bruce Tang <bruce@brucetangdesign.com>
2022-04-20 12:50:38 -0400

    desktop footer complete

---
+++ app/footer.php
@@ -0,0 +1,43 @@
+<footer>
+  <div id="footer-contents" class="container-fluid">
+    <div class="container p-0 d-flex justify-content-between m-auto">
+      <div class="footer-business-info">
+        <div class="business-info-container">
+          <div class="business-info-icon business-info-icon--location"></div>
+          <div class="business-info-copy">Model Electronics<br>615 East Crescent Ave.<br>Ramsey, NJ 07446</div>
+        </div>
+        <div class="business-info-container">
+          <div class="business-info-icon business-info-icon--phone"></div>
+          <div class="business-info-copy">(800)-433-9657<br>(201)-961-6200</div>
+        </div>
+        <div class="business-info-container">
+          <div class="business-info-icon business-info-icon--hours"></div>
+          <div class="business-info-copy">8AM - 8PM EST</div>
+        </div>
+      </div>
+      <div class="footer-logo-nav">
+        <img id="footer-site-identity" src="images/logo-model-electronics.svg" width="181px" height="51px" alt="Model Electronics Logo">
+        <nav class="pt-3 pb-3">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Services</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About Us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contact Us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#" >Aftermarket Products</a>
+            </li>
+          </ul>
+        </nav>
+        <a class="btn btn-dark border-white shadow-none" href="#" >Dealer Login</a>
+      </div>
+      <div class="footer-awards">
+        <img src="images/top-workplaces-badge.png" width="81px" height="178.5px" alt="NJ.com Top Workplaces 2019 Badge">
+      </div>
+    </div>
+  </div>
+</footer>
--- app/index.php
+++ app/index.php
@@ -10,6 +10,10 @@
 
 <body>
   <?php include 'header.php'; ?>
+  <main style="min-heihgt: 400px;">
+content
+  </main>
+  <?php include 'footer.php'; ?>
   <!-- JavaScript Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
