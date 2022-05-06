ff53deee5c45c2502cf55f4ff07e832982ffe070
Bruce Tang <bruce@brucetangdesign.com>
2022-05-06 12:06:43 -0400

    About page complete

--- app/about-us.php
+++ app/about-us.php
@@ -83,6 +83,125 @@
       </div>
     </section>
     <!-- End Bonafindes -->
+    <!-- State-of-the-Art Facility -->
+  <section id="reputation" class="img-with-text-cols-section">
+    <div class="container">
+      <div>
+        <picture class="children-100">
+          <source srcset="images/model-electronics-outstanding-repuatation-ISO-certified.webp, images/model-electronics-outstanding-repuatation-ISO-certified@2x.webp 2x" width="728px" height="578px" type="image/webp" alt="Serving the Automotive Industry For Over 40 Years">
+          <source srcset="images/model-electronics-outstanding-repuatation-ISO-certified.jpg, images/model-electronics-outstanding-repuatation-ISO-certified@2x.jpg 2x" width="728px" height="578px" type="image/jpeg" alt="Serving the Automotive Industry For Over 40 Years"> 
+          <img src="images/model-electronics-outstanding-repuatation-ISO-certified.jpg" width="728px" height="528px" alt="Serving the Automotive Industry For Over 40 Years">
+        </picture>
+      </div>
+      <div class="content-container">
+        <h2 class="mb-3">State-of-the-Art Facility</h2>
+        <p>Model Electronics, Inc. is an ISO9001:2015 and ISO14001:2015 company that has worked hard to maintain the industry's highest quality as well as environmental control ratings. We are counted among the top authorized OEM sales and service centers for our customers.</p>
+        <p>Our technologically advanced facility enables us to do all our work in-house and process large volumes of repairs, allowing us greater control over the services we provide our customers and a faster turnaround.</p>
+        <div class="d-flex mt-3 flex-column flex-sm-row">
+          <div class="pe-0 pe-sm-4 text-center">
+            <img class="w-lg-100 h-auto mw-100" src="images/NSF-badge@2x.png" alt="NSF-ISR Badge" width="84px" height="104px">
+          </div>
+          <div class="ps-0 pe-0 ps-sm-2 pe-sm-2 mt-3 mb-3 mt-sm-0 mb-sm-0 text-center">
+            <img src="images/iso-9001-badge.svg" alt="ISO 9001 Certified Badge" width="126px" height="50px">
+          </div>
+          <div class="ps-0 ps-sm-4 mt-3 mt-sm-0 text-center">
+            <img src="images/iso-14001-badge.svg" alt="ISO 14001 Certified Badge" width="126px" height="50px">
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End State-of-the-Art Facility -->
+  <!-- Company History -->
+  <section id="company-history">
+    <h2 class="text-center">Company History</h2>
+    <div class="container container-thin d-flex flex-column justify-content-center">
+      <!-- History Item -->
+      <div class="history-item">
+        <div class="history-img-container">
+          <picture>
+            <source srcset="images/model-electronics-timeline-1976.webp, images/model-electronics-timeline-1976@2x.webp 2x" width="396px" height="276px" type="image/webp" alt="1976 The first Model Electronics facility opens">
+            <source srcset="images/model-electronics-timeline-1976.jpg, images/model-electronics-timeline-1976@2x.jpg 2x" width="396px" height="276px" type="image/jpeg" alt="1976 The first Model Electronics facility opens"> 
+            <img src="images/model-electronics-timeline-1976.jpg" width="396px" height="276px" alt="1976 The first Model Electronics facility opens">
+          </picture>
+        </div>
+        <div class="history-caption">
+          <h4 class="history-year">1976</h4>
+          <p class="history-text">The first Model Electronics facility opens</p>
+        </div>
+      </div>
+      <!-- History Item -->
+      <div class="history-item img-on-right">
+        <div class="history-img-container">
+          <img src="images/model-electronics-timeline-1993.svg" width="239px" height="128px" alt="1993 Model Electronics expands by moving into a larger building at the same location">
+        </div>
+        <div class="history-caption">
+          <h4 class="history-year">1993</h4>
+          <p class="history-text">Model Electronics expands by moving into a larger building at the same location</p>
+        </div>
+      </div>
+      <!-- History Item -->
+      <div class="history-item">
+        <div class="history-img-container">
+          <picture>
+            <source srcset="images/model-electronics-timeline-1994-iso-certification.webp, images/model-electronics-timeline-1994-iso-certification@2x.webp 2x" width="396px" height="276px" type="image/webp" alt="1994 Model Electronics earns ISO certification">
+            <source srcset="images/model-electronics-timeline-1976.jpg, images/model-electronics-timeline-1994-iso-certification@2x.jpg 2x" width="396px" height="276px" type="image/jpeg" alt="1994 Model Electronics earns ISO certification"> 
+            <img src="images/model-electronics-timeline-1994-iso-certification.jpg" width="396px" height="276px" alt="1994 Model Electronics earns ISO certification">
+          </picture>
+        </div>
+        <div class="history-caption">
+          <h4 class="history-year">1994</h4>
+          <p class="history-text">Model Electronics earns ISO certification</p>
+        </div>
+      </div>
+      <!-- History Item -->
+      <div class="history-item img-on-right">
+        <div class="history-img-container">
+          <img src="images/model-electronics-timeline-1995.svg" width="320px" height="174px" alt="1995 Model Electronics moves into a 30,000 square foot facility in Spring Valley, NY">
+        </div>
+        <div class="history-caption">
+          <h4 class="history-year">1995</h4>
+          <p class="history-text">Model Electronics moves into a 30,000 square foot facility in Spring Valley, NY</p>
+        </div>
+      </div>
+      <!-- History Item -->
+      <div class="history-item">
+        <div class="history-img-container">
+          <img src="images/model-electronics-timeline-2002.svg" width="236px" height="174px" alt="2002 Model Electronics becomes the last GM service center in the Northeast">
+        </div>
+        <div class="history-caption">
+          <h4 class="history-year">2002</h4>
+          <p class="history-text">Model Electronics becomes the last GM service center in the Northeast</p>
+        </div>
+      </div>
+      <!-- History Item -->
+      <div class="history-item img-on-right">
+        <div class="history-img-container">
+          <picture>
+            <source srcset="images/model-electronics-timeline-2005-ramsey-nj.webp, images/model-electronics-timeline-2005-ramsey-nj@2x.webp 2x" width="495px" height="282px" type="image/webp" alt="2005 The first Model Electronics facility opens">
+            <source srcset="images/model-electronics-timeline-2005-ramsey-nj.jpg, images/model-electronics-timeline-2005-ramsey-nj@2x.jpg 2x" width="495px" height="282px" type="image/jpeg" alt="2005 The first Model Electronics facility opens"> 
+            <img src="images/model-electronics-timeline-2005-ramsey-nj.jpg" width="495px" height="282px" alt="2005 The first Model Electronics facility opens">
+          </picture>
+        </div>
+        <div class="history-caption">
+          <h4 class="history-year">2005</h4>
+          <p class="history-text">Model Electronics moves into a 65,000 square foot facility in Ramsey, NJ</p>
+        </div>
+      </div>
+      <!-- History Item -->
+      <div class="history-item">
+        <div class="history-img-container">
+          <img src="images/model-electronics-timeline-2022-largest-gm-service-center-usa.svg" width="475px" height="303px" alt="2022 Model Electronics is the largest GM service center in the USA">
+        </div>
+        <div class="history-caption">
+          <h4 class="history-year">2022</h4>
+          <p class="history-text">Model Electronics is the largest GM service center in the USA</p>
+        </div>
+      </div>
+      <!-- End History Items -->
+    </div>
+  </section>
+  <!-- End Company History -->
   </main>
   <?php include 'footer.php'; ?>
   <!-- JavaScript Bundle with Popper -->
